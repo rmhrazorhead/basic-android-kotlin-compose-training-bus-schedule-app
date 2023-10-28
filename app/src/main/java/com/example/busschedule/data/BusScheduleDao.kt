f7eff9bea8d862b2412574c68a7624724ff1078b
Reece Holzhauser <74988573+rmhrazorhead@users.noreply.github.com>
2023-10-27 18:31:26 -0700

    Test

---
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -0,0 +1,34 @@
+package com.example.busschedule.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BusScheduleDao {
+//    @Insert(onConflict = OnConflictStrategy.IGNORE)
+//    suspend fun insert(schedule: BusSchedule)
+//
+//    @Update
+//    suspend fun update(schedule: BusSchedule)
+//
+//    @Delete
+//    suspend fun delete(schedule: BusSchedule)
+
+    @Query(
+        """
+        SELECT * FROM schedule
+        WHERE stop_name = :stopName
+        ORDER BY arrival_time ASC
+        """
+    )
+    fun getByStopName(stopName: String): Flow<List<BusSchedule>>
+
+    @Query(
+        """
+        SELECT * FROM schedule
+        ORDER BY arrival_time ASC
+        """
+    )
+    fun getAll(): Flow<List<BusSchedule>>
+}
